ea3f6187242219ecd5caeb4c75e442766d0989a1
kaisheng deng <carmen.deng12@gmail.com>
2022-02-14 20:49:59 -0500

    phase 1 and phase 2 complete,be able to register new pokemon

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { append } = require('express/lib/response');
+const { append, redirect, type } = require('express/lib/response');
 const pg = require('pg');
 const Sequelize = require('sequelize');
 
@@ -33,19 +33,21 @@ const Pokemon = db.define('pokemon',{
 
 const syncAndSeed = async()=>{
     try{
-        const fire = await Type.create({name:'fire'});
-        const grass = await Type.create({name:'grass'});
-        const water = await Type.create({name:'water'});
-        const bulbasaur = await Pokemon.create({name:'bulbasaur',typeId:grass.id});
-        const squirtle = await Pokemon.create({name:'squirtle',typeId:water.id});
-        const charmander = await Pokemon.create({name:'charmander',typeId:fire.id});
+        const fire = await Type.create({name:'Fire'});
+        const grass = await Type.create({name:'Grass'});
+        const water = await Type.create({name:'Water'});
+        const bulbasaur = await Pokemon.create({name:'Bulbasaur',typeId:grass.id});
+        const squirtle = await Pokemon.create({name:'Squirtle',typeId:water.id});
+        const charmander = await Pokemon.create({name:'Charmander',typeId:fire.id});
     }catch(err){
         console.log(err)
     }
 }
-
+Pokemon.belongsTo(Type);
+//express
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({extended:false}))
 const init = async()=>{
     await db.authenticate();
     console.log('connected')
@@ -58,3 +60,83 @@ const init = async()=>{
 }
 
 init();
+
+//route 
+
+app.get('/',(req,res,next)=>{
+    res.redirect('pokemons')
+})
+
+app.post('/pokemons',async(req,res,next)=>{
+    try{
+        const pokemon = await Pokemon.create(req.body);
+        res.redirect(`/types/${pokemon.typeId}`)
+    }catch(err){
+        next(err)
+    }
+})
+
+
+app.get('/pokemons', async(req,res,next)=>{
+    try{
+        const pokemons = await Pokemon.findAll({
+            include:[Type]
+        });
+        const types = await Type.findAll();
+        const html = pokemons.map((pokemon)=>{
+            return `<ul class = '${pokemon.type.name}'><li>${pokemon.name} <a href = '/types/${pokemon.type.id}'>${pokemon.type.name}</a></li></ul>`
+        }).join('')
+        res.send(
+            `
+            <html>
+            <head>
+            </head>
+            <body>
+                <h4>POKEMONS THAT I EARN</h4>
+                <form method = 'POST'>
+                    <input name = 'name' placeholder = 'pokemon name'></input>
+                    <select name = 'typeId'>${types.map((type)=>{
+                        return `<option value = ${type.id}>${type.name}</option>`
+                    })}
+                    </select>
+                    <button>Register</button>
+                </form>
+                ${html}
+            </body>
+            </html>
+            `
+        )
+    }catch(err){
+        next(err);
+    }
+})
+
+app.get('/types/:id', async(req,res,next)=>{
+    try{
+        const type = await Type.findByPk(req.params.id)
+        const pokemons = await Pokemon.findAll(
+            {
+                where: {typeId : req.params.id}
+            }
+        )
+        const html = pokemons.map((pokemon)=>{
+            return `<ul><li>${pokemon.name}</li></ul>`
+        }).join('')
+        res.send(
+            `
+            <html>
+            <head>
+            </head>
+            <body>
+                <h4>POKEMONS THAT I EARN</h4>
+                <h4 class = ${type.name}>${type.name}</h4>
+                <a href ='/pokemons'>Back<a>
+                ${html}
+            </body>
+            </html>
+            `
+        )
+    }catch(err){
+        next(err);
+    }
+})
